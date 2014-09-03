5b41a2284a14d1375f77460ec2a7e02fc04ae1a9
dshamis317 <dshamis317@gmail.com>
2014-09-03 11:41:54 -0400

    added post to form

--- javascripts/app.js
+++ javascripts/app.js
@@ -18,8 +18,17 @@ function loadPageContent() {
   })
 }
 
-$(function() {
-  $('.container').load('home.html')
 
+function pageInitializers() {
+  $('html').hide();
+  $('.container').load('home.html');
+  $('html').fadeIn();
+}
+
+// URL FOR CONTACT FORM
+// http://forms.brace.io/andrey@differentencouters.org?name=Dmitry&_replyto=dshamis317%40gmail.com&subject=TEST&message=TEST+TEST+TEST+TEST+TEST
+
+$(function() {
+  pageInitializers()
   loadPageContent();
 })
