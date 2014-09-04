b25e94807c1df22e1c8f184c85a3c037e50ec738
dshamis317 <dshamis317@gmail.com>
2014-09-04 15:08:11 -0400

    added new donation language and load command for contact link

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,6 +11,7 @@ function displayBios() {
 }
 
 function loadPageContent() {
+  // Loads pages from the nav bar
   $('.toolbar li a').on('click', function(e) {
     e.preventDefault();
     var id = this.id;
@@ -24,13 +25,23 @@ function loadPageContent() {
     $('.container').load(id + '.html');
   })
 
+  // Loads Mosaic Partners page
   $(document).on('click', '#mosaic', function(e) {
     e.preventDefault();
-    var id = this.id;
-    $('.container').load(id + '.html');
+    $('.container').load('mosaic.html');
     setTimeout(function() {
       console.log('image carousel loaded');
       imageCarousel();
+    }, 100)
+  })
+
+  // Loads contact form from Donations page
+  $(document).on('click', '#contact_us', function(e) {
+    e.preventDefault();
+    $('.container').load('contact.html');
+    setTimeout(function() {
+      sendFormData();
+      console.log('send form data');
     }, 100)
   })
 }
@@ -78,9 +89,9 @@ function stickyFooter() {
   var windowHeight = $(window).height();
   if (windowHeight > bodyHeight) {
     $('.footer').css('position','absolute').css('bottom',0);
-}
+  }
 
-$(function() {
-  pageInitializers()
-  loadPageContent();
-})
+  $(function() {
+    pageInitializers()
+    loadPageContent();
+  })
